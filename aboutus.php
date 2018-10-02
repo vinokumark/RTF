e8480cfa13fe4800a424f396948588c00a61a591
Your Name <you@example.com>
2018-10-02 11:09:42 +0530

    testing

---
+++ aboutus.php
@@ -0,0 +1,52 @@
+<?php include"assest/common/doctype-link.php" ?>
+
+<?php include"assest/common/header.php" ?>
+
+<section class="first-section">
+    <div class="container">
+        <h1 class="text-center ">Abouts Us</h1>
+
+<p class="text-center">
+        We live in an age of unprecedented prosperity, but also unspeakable deprivation around the world because the world is increasingly divided. Some regions enjoy sustained levels of peace, security and prosperity, while others fall into seemingly endless cycles of conflict and violence. This is by no means inevitable and must be addressed.<br>
+We have seen many problems locally / globally and wanted to do something about it. Others share our concerns and want to do something. That’s why we have started a Non-Governmental Organization (NGO) in the name of “RAISE THE FLAG INTERNATIONAL FOUNDATION” to address the issues locally and globally with the following guiding principles.
+
+be global, independent, multicultural and non-party political
+use the best available scientific information to address issues and critically evaluate all its endeavors
+seek dialogue and avoid unnecessary confrontation
+build concrete conservation solutions through a combination of field-based projects, policy initiatives, capacity building and education work
+involve local communities and indigenous peoples in the planning and execution of its field programs, respecting their cultural as well as economic needs
+strive to build partnerships with other organizations, governments, business and local communities to enhance “Raise The Flag International Foundation” effectiveness
+run its operations in a cost-effective manner and apply donors’ funds according to the highest standards of accountability.
+
+Raise the Flag International Foundation (RTF), being a global Non-Governmental Organization (NGO) advocate for 
+1. Maintain Peace and Security
+2. Protect Human Rights
+3. Deliver Humanitarian Aid
+4. Promote Sustainable Development
+5. Uphold Law
+
+and dedicated to contributing for United Nations Sustainable Development Goals (SDG) at local, regional, national and international levels.
+Its membership is comprised of individuals, educators and NGO’s throughout the world who are actively forwarding the knowledge and skills for SDG by and for all Mankind.
+These 9 goals represent “Raise the Flag International Foundation” contribution to UN & Indian Sustainable Development Goals Plan targets:
+
+1. SDG 1 – No Poverty
+End poverty in all its forms everywhere
+2. SDG 2 – Zero Hunger
+End hunger, achieve food security and improved nutrition and promote sustainable agriculture
+3. SDG 3 – Good Health and Well-being for People.
+Ensure healthy lives and promote well-being for all at all ages
+4. SDG 4 – Quality Education
+Ensure inclusive and equitable quality education and promote lifelong learning opportunities for all
+5. SDG 5 – Gender Equality
+Achieve gender equality and empower all women and girls
+6. SDG 6 – Clean Water and Sanitation
+Ensure availability and sustainable management of water and sanitation for all
+7. SDG 8 – Decent work and economic Growth.
+Promote sustained, inclusive and sustainable economic growth, full and productive employment and decent work for all
+8. SDG 16 – Peace, Justice and strong intuition.
+Promote peaceful and inclusive societies for sustainable development, provide access to justice for all and build effective, accountable and inclusive institutions at all levels
+9. SDG 17 – Partnership for Goals
+Strengthen the means of implementation and revitalize the global partnership for sustainable development
+</p>
+</div>
+</section>
